693ed61ca4c3a25f31a02605b334e31e61fbc23f
MoisesKdev <moisesk.dev@outlook.com>
2022-04-25 16:02:09 -0300

    Adicionado funçao de deposito

--- src/Conta.php
+++ src/Conta.php
@@ -5,4 +5,23 @@ class conta
    public string $cpfTitular;
    public string $nomeTitular;
    public float $saldo;
+
+    public function sacar(float $valorASacar)
+    {
+        if ($valorASacar > $this->saldo){
+            echo 'Saldo Indisponível';
+        }else {
+            $this->saldo -= $valorASacar;
+        }
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0){
+            echo "valor precisa ser positivo";
+        }else{
+        $this->saldo += $valorADepositar;
+        }
+    }
+
 }
