7d55b3c37303bea60cacfbd4b1a9cdea91643414
MoisesKdev <moisesk.dev@outlook.com>
2022-04-25 16:17:08 -0300

    Acrescentando transferencia e melhorando leitura do código

--- src/Conta.php
+++ src/Conta.php
@@ -4,24 +4,40 @@ class conta
 {
    public string $cpfTitular;
    public string $nomeTitular;
-   public float $saldo;
+   public float $saldo = 0;
 
     public function sacar(float $valorASacar)
     {
         if ($valorASacar > $this->saldo){
             echo 'Saldo Indisponível';
-        }else {
-            $this->saldo -= $valorASacar;
+            return;
         }
+        
+        $this->saldo -= $valorASacar;
+        
     }
 
     public function depositar(float $valorADepositar): void
     {
         if ($valorADepositar < 0){
             echo "valor precisa ser positivo";
-        }else{
-        $this->saldo += $valorADepositar;
+            return;
         }
+
+        $this->saldo += $valorADepositar;
+        
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo){
+            echo "Saldo Indisponível";
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+
     }
 
 }
