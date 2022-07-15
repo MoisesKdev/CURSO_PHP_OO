d688a0d3cb165599963f113feb3a1e82d96093f0
Moisés Kalebe <moiseskdev@outlook.com.br>
2022-07-15 17:48:10 -0300

    100% funcional

--- PROJETO_IMC/index.php
+++ PROJETO_IMC/index.php
@@ -1,13 +1,16 @@
 <?php
-
+//Incluí obrigatóriamente os arquivos
 require_once ("/home/moises/Documentos/CURSO_PHP_OO/PROJETO_IMC/src/classes.php");
 require_once("/home/moises/Documentos/CURSO_PHP_OO/PROJETO_IMC/src/function.php");
 
+
+//Cria uma variável Pessoa1 com tipo Pessoa > definido em classes.php
 $Pessoa1 = new Pessoa;
 $Pessoa1->nomePessoa = "Moisés";
 $Pessoa1->alturaPessoa = "1.80";
-$Pessoa1->pesoPessoa = "20";
+$Pessoa1->pesoPessoa = "110";
 
+//exibe retorno da função calculoImc() > definida em classes.php
 echo $Pessoa1->calculoImc();
 
 echo PHP_EOL;
--- PROJETO_IMC/src/classes.php
+++ PROJETO_IMC/src/classes.php
@@ -7,12 +7,28 @@ class Pessoa{
 
     
     public function calculoImc(){
-      $imcPessoa = $this->pesoPessoa/($this->alturaPessoa*$this->alturaPessoa);
-        return $imcPessoa;
+        $imcPessoa = $this->pesoPessoa/($this->alturaPessoa*$this->alturaPessoa);
+            if($imcPessoa < 18.5){
+                echo "Seu imc é {$imcPessoa} você está Abaixo do Peso" . PHP_EOL;
+            }
+            if($imcPessoa >= 18.5 && $imcPessoa <= 24.9){
+                echo "Seu imc é {$imcPessoa} você está com Peso Normal" . PHP_EOL;
+            }
+            if($imcPessoa >= 25 && $imcPessoa <= 29.9){
+                echo "Seu imc é {$imcPessoa} você está Sobrepeso" . PHP_EOL;
+            }
+            if($imcPessoa >= 30 && $imcPessoa <= 39.9){
+                echo "Seu imc é {$imcPessoa} você está OBESO" . PHP_EOL;
+            }
+            if($imcPessoa >= 40){
+                echo "Seu imc é {$imcPessoa} você está com Obesidade Mórbida" . PHP_EOL;
+            }
     }
+   
 
     
 }
+
 
 
 
